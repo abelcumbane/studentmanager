5ea442385752f690ec381f87940b7744f3555708
Abel Cumbane <abelrosac@gmail.com>
2025-03-04 13:28:46 +0100

    chore: Impl JPQL para buscar dados na BD

--- src/main/java/com/mapulassapp/repositorys/StudentRepository.java
+++ src/main/java/com/mapulassapp/repositorys/StudentRepository.java
@@ -1,12 +1,19 @@
 package com.mapulassapp.repositorys;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mapulassapp.models.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+	//JPQL to find all the students that includes the substring the user defined.
+	@Query("select s from Student s where lower(s.name)  like lower(concat('%', :substring, '%'))")
+	List<Student> findStudents(String substring);
 	
 	//All the CRUD operations are implemented by default
 
