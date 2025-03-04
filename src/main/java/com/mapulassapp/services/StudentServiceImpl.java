fb94e28ca71d8030c5677440cc4b44d2db15050e
Abel Cumbane <abelrosac@gmail.com>
2025-03-04 13:27:36 +0100

    feat: Impl da logica de StudentService

---
+++ src/main/java/com/mapulassapp/services/StudentServiceImpl.java
@@ -0,0 +1,38 @@
+package com.mapulassapp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mapulassapp.models.Student;
+import com.mapulassapp.repositorys.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+	
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Override
+	public void save(Student student) {
+		studentRepository.save(student);
+		
+	}
+
+	@Override
+	public void remove(Student student) {
+		studentRepository.delete(student);	
+	}
+
+	@Override
+	public List<Student> findAll() {
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public List<Student> find(String substring) {
+		return studentRepository.findStudents(substring);
+	}
+
+}
