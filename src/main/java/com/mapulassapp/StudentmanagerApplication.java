981198a4ae9dddcc8324f79cd0ea62c332463894
Abel Cumbane <abelrosac@gmail.com>
2025-03-04 08:01:03 +0100

    criacao de projecto

---
+++ src/main/java/com/mapulassapp/StudentmanagerApplication.java
@@ -0,0 +1,18 @@
+package com.mapulassapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@SpringBootApplication
+@Theme(themeClass = Lumo.class)
+public class StudentmanagerApplication implements AppShellConfigurator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapulassapp/StudentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapulassapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
