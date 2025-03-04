6baf2cb6ca21bf831a4e987d3223bc8dbd3c2dc3
Abel Cumbane <abelrosac@gmail.com>
2025-03-04 08:02:22 +0100

    chore: Entity Status created

---
+++ src/main/java/com/mapulassapp/models/Status.java
@@ -0,0 +1,49 @@
+package com.mapulassapp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Status {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column
+	private String name;
+	
+	public Status() {
+		
+	}
+
+	public Status(String name) {
+		this.name = name;
+	}
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+}
