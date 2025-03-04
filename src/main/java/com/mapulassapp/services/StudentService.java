549414df74c8ddf60738cc4a4681b996e2ff1e87
Abel Cumbane <abelrosac@gmail.com>
2025-03-04 13:26:37 +0100

    chore: Impl da interface StudentService

--- src/main/java/com/mapulassapp/services/StudentService.java
+++ src/main/java/com/mapulassapp/services/StudentService.java
@@ -1,5 +1,14 @@
 package com.mapulassapp.services;
 
+import java.util.List;
+
+import com.mapulassapp.models.Student;
+
 public interface StudentService {
+	
+	public void save(Student student);
+	public void remove(Student student);
+	public List<Student> findAll();
+	public List<Student> find(String substring);
 
 }
