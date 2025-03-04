7569e0fe4b22749b37b0865eec776a35e8defccf
Abel Cumbane <abelrosac@gmail.com>
2025-03-04 08:02:50 +0100

    chore: Entity Student created

---
+++ src/main/java/com/mapulassapp/models/Student.java
@@ -0,0 +1,86 @@
+package com.mapulassapp.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private int age;
+	
+	@Column
+	private int zipCode;
+	
+	@Column
+	private String country;
+	
+	@ManyToOne
+	@JoinColumn
+	private Status status;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name, int age, int zipCode, String country, Status status) {
+		this.name = name;
+		this.age = age;
+		this.zipCode = zipCode;
+		this.country = country;
+		this.status = status;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}	
+
+}
