60cb730699d3049af424d7785d8228bebe179b0a
Abel Cumbane <abelrosac@gmail.com>
2025-03-06 05:01:01 +0100

    feat: Impl dos metodos update e findById

--- src/main/java/com/mapulassapp/services/StudentServiceImpl.java
+++ src/main/java/com/mapulassapp/services/StudentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mapulassapp.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,5 +35,20 @@ public class StudentServiceImpl implements StudentService {
 	public List<Student> find(String substring) {
 		return studentRepository.findStudents(substring);
 	}
+
+	@Override
+    public void update(Student student) {
+        if (studentRepository.existsById(student.getId())) {
+            studentRepository.save(student);
+        } else {
+            throw new RuntimeException("Student not found");
+        }
+    }
+
+    @Override
+    public Student findById(Integer studentId) {
+        Optional<Student> student = studentRepository.findById(studentId);
+        return student.orElse(null);
+    }
 
 }
