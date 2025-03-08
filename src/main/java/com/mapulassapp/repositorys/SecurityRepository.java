edb242693fedb41a45d41bd01d468e3d2aec00fa
Abel Cumbane <abelrosac@gmail.com>
2025-03-08 14:09:18 +0100

    Actualizacao de findByUsername

--- src/main/java/com/mapulassapp/repositorys/SecurityRepository.java
+++ src/main/java/com/mapulassapp/repositorys/SecurityRepository.java
@@ -2,12 +2,13 @@ package com.mapulassapp.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mapulassapp.models.User;
 
 public interface SecurityRepository extends JpaRepository<User, Integer>{
 	
 	@Query("select u from User u where u.username=:username")
-	User findByUsername(String username);
+	User findByUsername( @Param("username") String username);
 
 }
